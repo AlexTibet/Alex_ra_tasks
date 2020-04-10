bf2d5e9da3af45b9938164b2b5b283ef9e5fc531
AlexSobko <alexfreetibet@gmail.com>
2020-04-10 15:33:40 +0700

    10.04.20 The Cyrillic alphabet has been removed all messages are translated into English.

--- ra_sq_eq_sol_02.py
+++ ra_sq_eq_sol_02.py
@@ -3,33 +3,31 @@ from math import sqrt
 
 def ra_sq_eq_sol(a, b, c):
     if a == 0:
-        print('Когда а = 0 уравнение не является квадратным.'
-              '\nПопробуйте снова')
+        print('When a = 0, the equation is not square.')
         return
     if b == 0 and c == 0:
-        print("Для неполного квадратного уравнения где 'b' и 'c' равны нулю "
-              "\nесть только один корень: x = 0"
-              '\nПопробуйте снова')
+        print("For an incomplete square equation where 'b' and 'c' are equal to zero"
+              "\nthere is only one root: x = 0.")
         return
     D = (b ** 2) - (4 * a * c)
     if D < 0:
-        print('У этого квадратного уравнения нет действительных корней.'
-              '\nПопробуйте снова')
+        print('This square equation has no actual roots.')
     elif D == 0:
         x = -b / (2 * a)
-        print('Единственный корень этого уравнения: x =', x)
+        print('The only root of this equation: x =', x)
     else:
         x1 = (-b + sqrt(D)) / (2 * a)
         x2 = (-b - sqrt(D)) / (2 * a)
-        print('Корни квадратного уравнения:'
+        print('The roots of the square equation:'
               '\nx1 =', x1,
               '\nx2 =', x2)
 
 
 if __name__ == '__main__':
-    a = int(input("Введите a = "))
-    b = int(input("Введите b = "))
-    c = int(input("Введите c = "))
+    print('Starts the function for solving square equations:')
+    a = int(input("Enter a = "))
+    b = int(input("Enter b = "))
+    c = int(input("Enter c = "))
     ra_sq_eq_sol(a, b, c)
 
 
